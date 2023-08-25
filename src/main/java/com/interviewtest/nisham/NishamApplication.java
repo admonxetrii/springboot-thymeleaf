3034db07170d3b24f6189a4058dcd75161922f95
Nisham Wagle <nishamwagle@gmail.com>
2023-08-25 11:01:58 +0545

    init repo

---
+++ src/main/java/com/interviewtest/nisham/NishamApplication.java
@@ -0,0 +1,13 @@
+package com.interviewtest.nisham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NishamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NishamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interviewtest/nisham/NishamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interviewtest.nisham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NishamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
